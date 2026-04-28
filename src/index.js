2dc02611bcf40d1b3b11b1f07839614945faefcb
Ultron-18 <isahedore@gmail.com>
2026-04-28 17:46:05 +0100

    fix(server): add trust proxy for rate limiter

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const { requireApiVersion } = require('./middleware/apiVersion.middleware');
 const { authLimiter, apiLimiter } = require('./middleware/rateLimiter.middleware');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors());
